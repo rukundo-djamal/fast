bde9b11022932a51f20308f0c5693ebd9b23015c
rukundo-djamal <djamalrknd@gmail.com>
2019-02-11 22:02:07 +0200

    fast

--- src/App.vue
+++ src/App.vue
@@ -52,9 +52,9 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar color="blue darken-3" dark app :clipped-left="$vuetify.breakpoint.mdAndUp" fixed>
-      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+      <v-toolbar-title style="width: 400px" class="ml-0 pl-3">
         <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <span class="hidden-sm-and-down">Fast</span>
+        <span class="hidden-sm-and-down">Premier Transport Services</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn icon>
